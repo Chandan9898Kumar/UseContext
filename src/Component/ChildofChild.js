91788f0e09ffba5676de4fa53f2b8c62db6c914e
Chandan <Chandan17ee@gmail.com>
2022-09-12 23:15:12 +0530

    Added New Component

---
+++ src/Component/ChildofChild.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { UserDetails } from "../MainComponent";
+import { useContext } from "react";
+const ChildofComponent=()=> {
+var FetchedData=useContext(UserDetails)
+console.log(FetchedData,"data")
+return (
+    <>
+    <h2>This is Child of child component,Here we are accessing data which was passed to parent 
+     Component as props.</h2>
+     <label>Name</label>
+      : {FetchedData.name}<br /><br />
+     <label>Age</label>
+      : {FetchedData.Age}
+    </>
+)
+}
+export default ChildofComponent;
--- src/Component/Parent.js
+++ src/Component/Parent.js
@@ -1,11 +1,13 @@
 import React from "react"
-import ChildComponent from '../Component/Child'
+import ChildComponent from '../Component/Child';
+import ChildofComponent from '../Component/ChildofChild'
 const ParentComponents=(props)=> {
 return(
     <>
     <h2>This is Parent component, Here instead of passing data as a props,child component
     Can Directly fetch all data by using useContext</h2><br /><br /><hr />
-    <ChildComponent />
+    <ChildComponent /><hr />
+    <ChildofComponent />
 
     </>
 )
